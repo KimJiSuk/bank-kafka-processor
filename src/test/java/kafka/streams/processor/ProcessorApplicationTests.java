305f31c576398bd31e6b220ff7823a06f4abd44a
kimjisuk <yes0229@gmail.com>
2021-06-22 19:37:18 +0900

    init project

---
+++ src/test/java/kafka/streams/processor/ProcessorApplicationTests.java
@@ -0,0 +1,13 @@
+package kafka.streams.processor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProcessorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
